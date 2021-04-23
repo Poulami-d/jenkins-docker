23d2e8e34b8857e0fa1546e6e2221104c91404d0
Poulami-d <Poulami-d>
2021-04-23 12:05:39 +0530

    initial commit

---
+++ src/main/java/com/example/memberservice/exception/CustomException.java
@@ -0,0 +1,10 @@
+package com.example.memberservice.exception;
+
+public class CustomException extends Exception{
+    private String errorMessage;
+
+    public CustomException(String errorMessage) {
+        super(errorMessage);
+        this.errorMessage = errorMessage;
+    }
+}
